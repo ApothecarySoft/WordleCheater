32554592bbe0c3f85749237e2cbe4d40ecac551c
winter <rrcaseyr@gmail.com>
2025-11-11 15:52:34 -0500

    optimized?

--- wordlecheater.py
+++ wordlecheater.py
@@ -1,6 +1,9 @@
 import sys
 import re
 import random
+import argparse
+from multiprocessing import Pool
+import itertools
 
 class Place:
 
@@ -53,43 +56,115 @@ def patternToRegex(pattern):
             regex += val.getCharacters()
     return regex
 
-test = True if len(sys.argv) > 1 and sys.argv[1] == "-t" else False
+def filterList(list, regex, containsAtLeast, containsAtMost):
+    return [line for line in lines if (all(line.count(a) >= x for (a, x) in containsAtLeast.items()) and re.search(regex, line) and all(line.count(a) <= x for (a, x) in containsAtMost.items()))]
+
+def theOldWay(test, lines):
+    random.shuffle(lines)
+
+    patternStructure = [Place() for i in range(5)]
+    containsAtMost = {chr(a): 5 for a in range(ord('a'), ord('z') + 1)}
+    containsAtLeast = {chr(a): 0 for a in range(ord('a'), ord('z') + 1)}
+
+    userIn = ""
+
+    if test:
+        print(lines[0])
+
+    while(True):
+        userIn = input("" if test else "Enter a word or type \"exit\"\n")
+
+        if userIn.lower() == "exit":
+            exit()
+
+        wordIn = ''.join(char for char in userIn.strip().lower() if char.isalpha())
+
+        userIn = input("" if test else "Enter the pattern\n0 for grey, 1 for yellow, 2 for green\nex: 02110\n")
+        patternIn = [int(a) for a in list(userIn.strip()) if a.isdecimal()]
+        
+        patternStructure, newLeast, newMost = getPattern(wordIn, patternIn, patternStructure)
+        containsAtLeast = {a: max(x, containsAtLeast[a]) for (a, x) in newLeast.items()}
+        containsAtMost = {a: min(x, containsAtMost[a]) for (a, x) in newMost.items()}
+        regex = patternToRegex(patternStructure)
+        filteredLines = filterList(lines, regex, containsAtLeast, containsAtMost)
+        if not test:
+            print(regex)
+            print(containsAtLeast)
+            print(containsAtMost)
+            print(*filteredLines, sep='\n')
+        else:
+            print(filteredLines[0], flush=True)
+
+def allCombinations():
+    return ["".join(a) for a in itertools.product([str(i) for i in range(3)], repeat=5)]
+
+def smartWorker(item):
+    global lines
+    totalPossibilities = 0
+    for combination in combinations:
+        patternIn = [int(a) for a in list(combination.strip()) if a.isdecimal()]
+        patternStructure = [Place() for i in range(5)]
+        containsAtMost = {chr(a): 5 for a in range(ord('a'), ord('z') + 1)}
+        containsAtLeast = {chr(a): 0 for a in range(ord('a'), ord('z') + 1)}
+        patternStructure, containsAtLeast, containsAtMost = getPattern(item, patternIn, patternStructure)
+        regex = patternToRegex(patternStructure)
+        filteredList = filterList(lines, regex, containsAtLeast, containsAtMost)
+        totalPossibilities += len(filteredList)
+    return [item, totalPossibilities]
+
+def smartMode():
+    global lines
+
+    random.shuffle(lines)
+
+    patternStructure = [Place() for i in range(5)]
+    containsAtMost = {chr(a): 5 for a in range(ord('a'), ord('z') + 1)}
+    containsAtLeast = {chr(a): 0 for a in range(ord('a'), ord('z') + 1)}
+
+    userIn = ""
+
+    while(True):
+        userIn = input("Enter a word or type \"exit\"\n")
+
+        if userIn.lower() == "exit":
+            exit()
+
+        wordIn = ''.join(char for char in userIn.strip().lower() if char.isalpha())
+
+        userIn = input("Enter the pattern\n0 for grey, 1 for yellow, 2 for green\nex: 02110\n")
+        patternIn = [int(a) for a in list(userIn.strip()) if a.isdecimal()]
+        
+        patternStructure, newLeast, newMost = getPattern(wordIn, patternIn, patternStructure)
+        containsAtLeast = {a: max(x, containsAtLeast[a]) for (a, x) in newLeast.items()}
+        containsAtMost = {a: min(x, containsAtMost[a]) for (a, x) in newMost.items()}
+        regex = patternToRegex(patternStructure)
+        lines = filterList(lines, regex, containsAtLeast, containsAtMost)
+
+        print(regex)
+        print(containsAtLeast)
+        print(containsAtMost)
+
+        with Pool() as pool:
+            optimals = sorted(pool.map(smartWorker, lines), key=lambda item: -item[1])
+
+        print(*optimals, sep='\n')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--test", "-t", action='store_true')
+parser.add_argument('--new', '-n', action='store_true')
+
+args = parser.parse_args()
+
+combinations = allCombinations()
+print(combinations)
 
 lines = []
 with open('words.txt', 'r') as file:
     lines = [line.strip() for line in file.readlines()]
 
-random.shuffle(lines)
+if not args.new:
+    theOldWay(args.test, lines)
+else:
+    smartMode()
 
-patternStructure = [Place() for i in range(5)]
-containsAtMost = {chr(a): 5 for a in range(ord('a'), ord('z') + 1)}
-containsAtLeast = {chr(a): 0 for a in range(ord('a'), ord('z') + 1)}
-
-userIn = ""
-
-if test:
-    print(lines[0])
-
-while(True):
-    userIn = input("" if test else "Enter a word or type \"exit\"\n")
-
-    if userIn.lower() == "exit":
-        exit()
-
-    wordIn = ''.join(char for char in userIn.strip().lower() if char.isalpha())
-
-    userIn = input("" if test else "Enter the pattern\n0 for grey, 1 for yellow, 2 for green\nex: 02110\n")
-    patternIn = [int(a) for a in list(userIn.strip()) if a.isdecimal()]
-    
-    patternStructure, newLeast, newMost = getPattern(wordIn, patternIn, patternStructure)
-    containsAtLeast = {a: max(x, containsAtLeast[a]) for (a, x) in newLeast.items()}
-    containsAtMost = {a: min(x, containsAtMost[a]) for (a, x) in newMost.items()}
-    regex = patternToRegex(patternStructure)
-    filteredLines = [line for line in lines if (all(line.count(a) >= x for (a, x) in containsAtLeast.items()) and re.search(regex, line) and all(line.count(a) <= x for (a, x) in containsAtMost.items()))]
-    if not test:
-        print(regex)
-        print(containsAtLeast)
-        print(containsAtMost)
-        print(*filteredLines, sep='\n')
-    else:
-        print(filteredLines[0], flush=True)
